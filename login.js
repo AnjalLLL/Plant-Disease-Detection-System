7a8fb27d6f4ce8b87bdf499b4dd4975d62b4032f
Ashmita1555 <ashmitatimalsina12@gmail.com>
2024-09-02 15:24:39 +0545

    loginForm
    resolve conflicts in loginForm

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
 function showModel(){
+<<<<<<< HEAD
      document.querySelector('.overlay').classList.add('showoverlay')
      document.querySelector('.login-form').classList.add('showloginform')
 }
@@ -27,3 +28,32 @@ document.addEventListener('DOMContentLoaded', function () {
   close.addEventListener("click",closeModel)
  });
  
+=======
+    document.querySelector('.overlay').classList.add('showoverlay')
+    document.querySelector('.login-form').classList.add('showloginform')
+}
+function closeModel(){
+    document.querySelector('.overlay').classList.remove('showoverlay')
+    document.querySelector('.login-form').classList.remove('showloginform')
+}
+
+ 
+document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.querySelector('#toggelePassword');
+    const passwordInput = document.querySelector('#myInput');
+
+    togglePassword.addEventListener('click', function (e) {
+        // Toggle the type attribute
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        // Toggle the eye icon
+        this.classList.toggle('fa-eye-slash');
+    });
+    var btnlogin=document.querySelector(".btn-login");
+ btnlogin.addEventListener("click",showModel)
+
+ var close=document.querySelector("span");
+ close.addEventListener("click",closeModel)
+});
+>>>>>>> e6c306a (loginForm)
